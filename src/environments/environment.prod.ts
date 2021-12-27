02f5ac34ea1833d6f4656c97aae9269f837a0004
Kumar Saptam <saptampro2003@gmail.com>
2021-12-27 18:57:39 +0530

    beemadukaan new updated keys.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,17 @@
 export const environment = {
   firebase: {
-    projectId: 'beema-dukaan',
-    appId: '1:107471374234:web:04d33f26d679dbffda9c23',
-    storageBucket: 'beema-dukaan.appspot.com',
-    apiKey: 'AIzaSyAd6BSsdMow7xMU11UEJ2k-oGd-nZ8WoBU',
-    authDomain: 'beema-dukaan.firebaseapp.com',
-    messagingSenderId: '107471374234',
-    measurementId: 'G-8J97RDXTR1',
+    apiKey: 'AIzaSyDYoTC50N48sHyRJ0rLCLc1MhO7JSC2FJI',
+    authDomain: 'beemadukaan.firebaseapp.com',
+    databaseURL:'https://beemadukaan-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'beemadukaan',
+    storageBucket: 'beemadukaan.appspot.com',
+    messagingSenderId: '876938924830',
+    appId: '1:876938924830:web:cff1e8f5ba64828a7bef0f',
+    measurementId: 'G-PSCK7KKCTN',
   },
   ContentfulConfig:{
     space: 'wl1z0pal05vy',
-    accessToken:
-      '0e3ec801b5af550c8a1257e8623b1c77ac9b3d8fcfc1b2b7494e3cb77878f92a',
-  
+    accessToken:'0e3ec801b5af550c8a1257e8623b1c77ac9b3d8fcfc1b2b7494e3cb77878f92a',
     contentTypeIds: {
       product: '2PqfXUJwE8qSYKuM0U6w8M',
     },
